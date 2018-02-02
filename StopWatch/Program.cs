d8b3a04133e48942855d330261f435dfb062e772
TRH\rhd5538 <tshuff@fbhealthplans.com>
2018-02-02 10:58:05 -0600

    initial commit

---
+++ StopWatch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool readyToExit = false;
+
+            var timer = new Timer();
+            Console.WriteLine("WELCOME TO STOPWATCH!!!");
+            Console.WriteLine("------------------");
+            Console.WriteLine("Type 'start' to begin timer");
+            Console.WriteLine("Type 'stop' to stop the timer and get elapsed time");
+            Console.WriteLine("Type 'quit' to exit the application");
+
+            while (!readyToExit)
+            {
+                string currentCommand = Console.ReadLine();
+
+                if (currentCommand.ToLower() == "start")
+                    timer.start();
+                else if (currentCommand.ToLower() == "stop")
+                    timer.stop();
+                else if (currentCommand.ToLower() == "quit")
+                    readyToExit = true;
+                else Console.WriteLine("Invalid Command");
+            }
+                
+        }
+    }
+}
---
+++ StopWatch/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StopWatch
+{
+    public class Timer
+    {
+        public DateTime startTime { get; private set; }
+        public DateTime stopTime { get; private set; }
+        public bool isRunning { get; private set; }
+        public TimeSpan elapsed { get; private set; }
+
+        public Timer()
+        {
+            isRunning = false;
+        }
+
+        public void start()
+        {
+            if (isRunning)
+                throw new InvalidOperationException();
+            else
+            startTime = DateTime.Now;
+            //Console.WriteLine(startTime);
+            isRunning = true;
+        }
+
+        public void stop()
+        {
+
+
+            stopTime = DateTime.Now;
+            //Console.WriteLine(stopTime);
+
+            //get the elapsed time as a timespan value.
+            TimeSpan ts = stopTime-startTime;
+
+            ////Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine("RunTime: " + elapsedTime);
+        }
+
+    }
+}
